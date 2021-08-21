8e00ab933f80920a1bfed024e4d0f6942e29f9ba
Tigris of Gaul <tigrisofgaul@protonmail.com>
2021-08-21 13:44:23 -0400

    Remove test

--- test/Contracts/tarot-core/CStorage.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.5.16;
-
-contract CStorage {
-    address public borrowable0;
-    address public borrowable1;
-    address public tarotPriceOracle;
-    uint256 public safetyMarginSqrt = 1.58113883e18; //safetyMargin: 250%
-    uint256 public liquidationIncentive = 1.04e18; //4%
-}
--- test/Contracts/tarot-core/interfaces/IBorrowTracker.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.5.0;
-
-interface IBorrowTracker {
-    function trackBorrow(
-        address borrower,
-        uint256 borrowBalance,
-        uint256 borrowIndex
-    ) external;
-}
